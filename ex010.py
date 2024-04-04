59d4a14bf60a91bd5e618a0f78dbbe62b569ec1d
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-04 10:58:22 -0300

    att

--- ex010.py
+++ ex010.py
@@ -1,6 +1,6 @@
 #Crie um programa que leia quanto dinheiro uma pessoa
 # tem na carteira e mostre quantos dólares ela pode comprar
 # (considere US$1,00=R$3,27)
-real = float(input("quantos R$ você tem ? "))
-dol = real / 3.27
-print("com R${} você consegue comprar US${}  ".format(real, dol))
+real = float(input("quantos R$ você tem ?  R$"))
+dolar = real / 3.27
+print("Com R${} você consegue comprar US${:.2f}  ".format(real, dolar))
--- ex011.py
+++ ex011.py
@@ -6,4 +6,4 @@ larg = float(input("Qual é a largura da parede:  "))
 alt = float(input("Qual é a altura da parede:  "))
 mt4 = larg * alt
 quant = mt4 * 0.5
-print("com {} metros de largura mais {} metros de altura serão = {}M² e sera necessario {} litros de tinta.".format(larg, alt, mt4, quant))
+print("com {} metros de largura  mais {} metros de altura serão = {}M² e sera necessario {} litros de tinta.".format(larg, alt, mt4, quant))
