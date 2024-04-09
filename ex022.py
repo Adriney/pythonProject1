dd8ed8d1d6467bbd7d3a486284a0bba93ed4c71e
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-09 12:22:23 -0300

    att

--- ex022.py
+++ ex022.py
@@ -3,3 +3,12 @@
 # => O nome com todas as letras minúsculas.
 # =>Quantas letras ao todo(sem considerar e espaços).
 # =>Quantas letras tem o primeiro nome.
+
+
+frase = input("Qual o seu nome:")
+print(frase.upper())
+print(frase.lower())
+print(len(frase.replace(" ", "")))
+frase = (frase.split()[0])
+print(len(frase))
+
--- ex023.py
+++ ex023.py
@@ -5,3 +5,8 @@
 #dezena:3
 #centena:8
 #milhar:1
+numero = input("Digite um número:")
+print("unidade:",numero[3])
+print("dezena:",numero[2])
+print("centena:",numero[1])
+print("milhar:",numero[0])
