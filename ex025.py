691743b12211c075278ff7671305ec2ed2a657d7
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-10 19:43:05 -0300

    Update ex025.py

--- ex025.py
+++ ex025.py
@@ -1,2 +1,9 @@
 #Crie um programa que leia o nome de uma pessoa
 # e diga se ela tem "SILVA" no nome.
+nome =input("Qual o seu nome:")
+nome = nome.upper()
+print("SILVA" in nome)
+if "SILVA" in nome:
+    print("Possui Silva no nome")
+else:
+    print("Não possui Silva no nome")
