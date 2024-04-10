2af3bde8a9d21b876a7b5383b6e584de7162caf0
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-10 19:43:28 -0300

    Update ex027.py

--- ex027.py
+++ ex027.py
@@ -1,6 +1,13 @@
 #Faça um programa que leia o nome completo de
 # uma pessoa, mostrando em seguida o primeiro
 # e o último nome separadamente.
-#Ex:Ana Maria de Souza
-#primeiro=Ana
-#íltimo= Souza
+#Ex: Ana Maria de Souza
+#Primeiro=Ana
+#Último= Souza
+nome = input("Qual o seu nome:")
+print(nome)
+nome1 = nome.split()[0]
+
+print("Primeira parte:", nome1)
+nome2 = nome.split()[-1]
+print("Última parte:", nome2)
