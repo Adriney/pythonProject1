33a4c83364871c9f8d405503b4b17d0365f2085c
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-10 19:43:16 -0300

    Update ex026.py

--- ex026.py
+++ ex026.py
@@ -1,4 +1,11 @@
-#Faça um programa que leia uma frase pelo teclado e moste:
+#Faça um programa que leia uma frase pelo teclado e mostee:
 #>Quantas vezes aparece a letra "A".
 #>Em que posição ela aparece a primeira vez.
 #>Em que posição ela aparece a última vez.
+frase = input("Digite sua frase:")
+frase=frase.upper()
+print(frase)
+print("Possui {} letras 'A' na frase!".format(frase.count("A")))
+print("primeira posição da letra 'A' é: {}".format(frase.find("A")+1))
+# +1 para ajustar a indexação (Python começa a indexação a partir de 0).
+print("Ultima  posição da letra 'A' é: {}".format(frase.rfind("A")+1))
