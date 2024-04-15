3e11d598d810e42491fb1c5fa69f1ed76986b782
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-15 17:50:09 -0300

    Update ex031c.py

--- ex031c.py
+++ ex031c.py
@@ -5,4 +5,4 @@
 distância = float(input("qual é a distância da sua viagem? "))
 print("Você está prestes a começar uma viagem de {}KM.".format(distância))
 preço = distância * 0.50 if distância <=200 else distância *0.45
-print("E o preço da sua passagem será de R${:.2f}".format(preço))
+print("E o preço da sua passagem será de \033[31mR${:.2f}".format(preço))
