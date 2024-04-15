d942cf80bd30949d2329a794049af4c0ef1bb1ad
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-15 17:50:00 -0300

    Update ex035.py

--- ex035.py
+++ ex035.py
@@ -1,9 +1,9 @@
 #Desenvolva um programa que leia o comprimento de três retas
 # e diga ao usuário se elas podem ou não formar um triângulo.(r1,r2,r3)
-r1 = float(input("Digite o comprimento da primeira reta:"))
-r2 = float(input("Digite o comprimento da segunda reta:"))
-r3 = float(input("digite o comprimento da terceira reta:"))
+r1 = int(input("Digite o comprimento da \033[32mprimeira\033[m reta:"))
+r2 = int(input("Digite o comprimento da \033[34msegunda\033[m reta:"))
+r3 = int(input("digite o comprimento da \033[36mterceira\033[m reta:"))
 if (r1 + r2 > r3) and (r1 +r3 > r2) and (r2 +r3 > r1):
-    print(" As medidas das retas {}, {}, {}, pode formar um triângulo".format(r1,r2,r3))
+    print(" As medidas das retas {}, {}, {}, \033[31mpode\033[m formar um triângulo".format(r1,r2,r3))
 else:
-    print("As medidas das retas {}, {}, {}, nao podem formar um triângulo".format(r1,r2,r3))
+    print("As medidas das retas {}, {}, {}, \033[31mnão\033[m podem formar um triângulo".format(r1,r2,r3))
