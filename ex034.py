954e83cf1bfd0798b0760c8937d900823b223bcf
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-15 17:49:47 -0300

    Update ex034.py

--- ex034.py
+++ ex034.py
@@ -2,13 +2,16 @@
 # e calcule o valor do seu aumento. Para salários superiores
 # a R$1.125,00 calcule um aumento de 10%.
 # Para inferiores ou iguais, o aumento é de 15%.
-salário = float(input("Digite o valor do seu salário R$"))
+cor = {'limpa': '\033[m',
+       'verde': '\033[32m'}
+salário = int(input("Digite o valor do seu salário R$"))
 #base = 1126
+
 aum1 = 0.10
 aum2 = 0.15
 ns1 = salário *(1 + aum1)
 ns2 = salário *(1 + aum2)
 if salário >= 1126 :
-    print("Seu  salário com 10% de aumento ficara em R${}".format(ns1))
+    print("Seu  salário com {}10% de aumento{} ficara  R${:.2f} !".format(cor['verde'],cor['limpa'],ns1))
 else:
-    print("Seu salário com aumento de 15%  ficara R${}".format(ns2))
+    print("Seu salário com {}aumento de 15%{}  ficara R${:.2f} !".format(cor['verde'],cor['limpa'],ns2))
