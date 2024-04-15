90ab372903aadbe0869c26451724ad08c7920dd7
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-15 17:50:39 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite um numero:'))
-n2 = int(input('Digite outro numero:'))
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
 s = n1 + n2
-print('A soma entre de {} + {}  é igual a {} !'.format(n1,n2,s))
+print('A soma entre de \033[31m{}\033[m + \033[31m{}\033[m  é igual a \033[34m{}\033[m !'.format(n1,n2,s))
