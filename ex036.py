9de579e336938114bcd69ad2fa6984d2f7c5e793
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-17 23:30:43 -0300

    Update ex036.py

--- ex036.py
+++ ex036.py
@@ -1,6 +1,26 @@
-#Escreva um programa para aprovar o empréstimo bancário para
+#Escreva um programa para aprovar um empréstimo bancário para
 # a compra de uma casa. O programa vai perguntar o valor da
-# casa, o salálario do comprador e em quantos anos ele vai
-# pagar. Calule o valor da prestação mensal, sabendo que ela
+# casa, o salário do comprador, e em quantos anos ele vai
+# pagar. Calule o valor da prestação mensal, sabendo que ele
 # não pode exceder 30% do salário ou então o empréstimo será negado.
+print("==="*11)
+print("Empréstimo bancário(para imóvel)")
+print("==="*11)
+casa = float(input("Qual o valor da casa:"))
+salario = float(input("Qual é o seu salário:"))
+prazo1 = int(input("Em quantos anos pretende pagar:"))
+prazo2 = prazo1 * 12
+parcela = casa / prazo2
+calc = salario -(salario * 70 /100)
 
+if calc >= parcela:
+    print("Na condição do salário de R${} com prazo de {} anos({} meses) "
+          "\na parcela fica R${:.2f} e o Banco \033[31mAPROVOU\033[m seu emprestimo "
+          "\npara aquisição de seu imovel! ".format(salario,prazo1,prazo2,parcela))
+elif calc < parcela:
+    print("Na condição do salário de R${} com prazo de {} anos({} meses) "
+          "\na parcela fica R${:.2f} e o Banco \033[31mNEGOU\033[m seu emprestimo "
+          "\n!".format(salario, prazo1, prazo2, parcela))
+else:
+    print("Não foi possivel identificar as condições necessarias para analise!")
+print("Parcela máxima nessa condição é {}".format(calc))
