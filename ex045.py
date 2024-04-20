a41cf378ed8f673281ce24b5f68ccb626c4c4215
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-19 21:20:56 -0300

    Create ex045.py

---
+++ ex045.py
@@ -0,0 +1,18 @@
+#Crie um programa que faça o computador jogar
+# Jokenpô com você(pedra/papel/tesoura).
+from random import choice
+n0 = str(input("Jokenpô!!! qual sua apósta:"))
+n1 = "pedra"
+n2 = "papel"
+n3 = "tesoura"
+lista = [n1,n2,n3]
+pc = choice(lista)
+print("\033[33mVocê escolheu {}\033[m, e o \033[35mcomputador escolheu {}\033[m".format(n0,pc))
+if  n0 == pc:
+    print("Empate")
+elif (n0 =="tesoura" and pc == "papel") or \
+      (n0 =="papel" and pc == "pedra") or \
+      (n0 =="pedra" and pc == "tesoura") :
+    print("Você venceu!!!")
+else:
+    print("Você perdeu")
