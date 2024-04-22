bc27e2f54a86cbdbf333f59f45b000bb3592ecf4
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-21 22:26:06 -0300

    Update ex038.py

--- ex038.py
+++ ex038.py
@@ -7,8 +7,8 @@ n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
 
 if n1>n2:
-    print("O \033[36mprimeiro\033[m  valor é \033[36mmaior\033[m! ")
+    print("O \033[36m primeiro\033[m  valor é \033[36m maior\033[m! ")
 if n1<n2:
-    print("O \033[33msegundo\033[m valor é \033[33mmaior\033[m!")
+    print("O \033[33m segundo\033[m valor é \033[33m maior\033[m!")
 if n1 == n2:
-    print("Não existe valor maior, \033[31mos dois são iguais\033[m!")
+    print("Não existe valor maior, \033[31m os dois são iguais \033[m!")
