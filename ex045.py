60f5d2cc33310b6faea3279fb159e09a8a2289ee
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-22 00:34:38 -0300

    Update ex045.py

--- ex045.py
+++ ex045.py
@@ -7,7 +7,7 @@ n2 = "papel"
 n3 = "tesoura"
 lista = [n1,n2,n3]
 pc = choice(lista)
-print("\033[33mVocê escolheu {}\033[m, e o \033[35mcomputador escolheu {}\033[m".format(n0,pc))
+print("\033[33m Você escolheu {}\033[m, e o \033[35m computador escolheu {}\033[m".format(n0,pc))
 if  n0 == pc:
     print("Empate")
 elif (n0 =="tesoura" and pc == "papel") or \
