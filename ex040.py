6a86ccb3ce9e07bb4f1cc54ff6e00bcaba699219
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-22 00:33:54 -0300

    Update ex040.py

--- ex040.py
+++ ex040.py
@@ -8,8 +8,8 @@ n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 m = (n1+n2)/2
 if m <=5.0:
-    print("Sua média foi {} / \033[31mREPROVADO\033[m!!!".format(m))
+    print("Sua média foi {} / \033[31m REPROVADO\033[m !!!".format(m))
 elif m >= 7:
-    print("Sua média foi {} /  \033[32mAPROVADO\033[m!!!".format(m))
+    print("Sua média foi {} /  \033[32m APROVADO\033[m !!!".format(m))
 else:
-    print("Sua média foi {} /  \033[34mRECUPERAÇÃO\033[m!!!".format(m))
+    print("Sua média foi {} /  \033[34m RECUPERAÇÃO\033[m !!!".format(m))
