2c15521e5b6614b5f535bd0859f5133098c1974c
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-22 00:34:12 -0300

    Update ex042.py

--- ex042.py
+++ ex042.py
@@ -16,6 +16,6 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
         tipo ="Isóceles"
     else:
         tipo = "Escaleno"
-    print("os segmentos acima PODEM FORMAR triângulo {}!".format(tipo))
+    print("os segmentos acima PODEM FORMAR um triângulo {}!".format(tipo))
 else:
     print("os segmentos acima NÃO PODEM FORMAR triângulo!")
