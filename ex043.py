4db5481497cfdfa7ce87c983055855a4fcd0d6e0
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-22 00:34:23 -0300

    Update ex043.py

--- ex043.py
+++ ex043.py
@@ -9,19 +9,19 @@
 print("-=-"*12)
 print("CALCULE SEU IMC")
 print("-=-"*12)
-peso = float(input("Qual o seu \033[31mpeso\033[m:"))
-altura = float(input("qual é a sua \033[31maltura\033[m:"))
+peso = float(input("Qual o seu \033[31m peso\033[m:"))
+altura = float(input("qual é a sua \033[31m altura\033[m:"))
 imc = (peso / (altura * altura))
 
 if  (imc < 18.5):
-    print("Seu IMC é {}, você está abaixo do peso!".format(imc))
-elif 18.5 < imc < 25 :
-    print("Seu IMC é {}, você está no peso ideal!".format(imc))
-elif 25 < imc < 30 :
-    print("Seu IMC é {}, você está com Sobrepeso!".format(imc))
-elif 30 < imc < 40:
-    print("Seu IMC é {}, você está com Obesidade!".format(imc))
+    print("Seu IMC é {:.1f}, você está abaixo do peso!".format(imc))
+elif 18.5 <= imc < 25 :
+    print("Seu IMC é {:.1f}, você está no peso ideal!".format(imc))
+elif 25 <= imc < 30 :
+    print("Seu IMC é {:.1f}, você está com Sobrepeso!".format(imc))
+elif 30 <= imc < 40:
+    print("Seu IMC é {:.2f}, você está com Obesidade!".format(imc))
 else:
     #(imc > 40): não precisa adicionar essa linha, pois a
     # unica opção que sobra é acima de imc 40
-    print("Seu IMC é {}, você está com Obesidade mórbida!!".format(imc))
+    print("Seu IMC é {:.1f}, você está com Obesidade mórbida, CUIDADO!".format(imc))
