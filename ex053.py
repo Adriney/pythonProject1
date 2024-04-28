1dc848e35bbc8d8894820ad1b3f2e95477689120
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-28 15:58:20 -0300

    Create ex053.py

---
+++ ex053.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia uma frase qualquer
+# e diga se ela é um palíndromeo, desconsiderando os espaços.
+# EX:APOS A SOPA / A SACADA DA CASA /A TORRE DA DERROTA /
+# O LOBO AMA BOLO / ANOTARAM A DATA DA MATATONA
