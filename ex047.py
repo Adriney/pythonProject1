8885985c085da3f2aa9be4e7ed8735575554bd7d
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-01 22:50:58 -0300

    Update ex047.py

--- ex047.py
+++ ex047.py
@@ -1,2 +1,13 @@
 #Crie um programa que mostre na tela todos os números
-# pares que wstão no intervalo entre 1 e 50.
+# pares que estão no intervalo entre 1 e 50.
+print('='*25)
+print('\033[34m Números pares de 1 a 50.\033[m')
+print('='*25)
+def num_par (numero):
+    pares = []
+    for i in range(2, numero +1, 2):
+       pares.append(i)
+    return pares
+resultado = num_par(50)
+#print('Os números \033[31m pares \033[m de 50 são :\n',resultado)
+print(resultado)
