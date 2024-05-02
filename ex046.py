4cb7d3226ec2c602747e8d75c435990995bcfedd
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-01 22:18:09 -0300

    Update ex046.py

--- ex046.py
+++ ex046.py
@@ -1,3 +1,17 @@
 #Faça um programa que mostre na tela uma contagem regressiva para
 # o estouro de fogos de artifício, indo de 10 até 0, com uma pausa
 # de 1 segundo entre eles.
+import time
+print('-='*15)
+print('CONTAGEM REGRESSIVA')
+print('-='*15)
+segundos = 10
+def cont_reg (segundos):
+    while segundos >=0:
+        print(segundos)
+        time.sleep(1)
+        segundos -=1
+
+    print('Fim da contagem!!!')
+cont_reg(10)
+
