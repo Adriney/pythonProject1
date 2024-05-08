7b13011d7db6240ea2018662de8540dbef4137d7
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-08 09:22:14 -0300

    Update ex048.py

--- ex048.py
+++ ex048.py
@@ -1,3 +1,11 @@
 #Faça um programa que calcule a soma entre todos os números
 # ímpares que são múltiplos de três e que se encontram no
 # intervalo de 1 até 500.
+soma = 0  #recebe a soma de todos os números
+cont = 0  #recebe a quantidade de números que foi somado
+for c in range(1, 501, 2):
+    if c % 3 == 0: #Números divisiveis por 3 igual a 0
+        cont += 1 #Contabiliza a quantidade de números que foi somado
+        soma += c #soma recebe o que ela tem mais o número.
+      #print(c, end=' ') Aparece os números de divisiveis por 3
+print(f'A soma de todos os {cont} valores solicitados é {soma}.')
