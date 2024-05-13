f8c6abfbdfede22f28354f4f02e126932498d6de
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-13 16:24:22 -0300

    Update ex050.py

--- ex050.py
+++ ex050.py
@@ -1,3 +1,25 @@
 #Desenvolva um programa que leia seis números inteiros e mostre
 # a soma apenas daqueles que forem pares.
 # Se o valor digitado for impar, desconsidere-o.
+num1 = int(input('Digite o 1° número:'))
+num2 = int(input('Digite o 2° número:'))
+num3 = int(input('Digite o 3° número:'))
+num4 = int(input('Digite o 4° número:'))
+num5 = int(input('Digite o 5° número:'))
+num6 = int(input('Digite o 6° número:'))
+
+soma_pares = 0
+
+if num1 % 2 == 0:
+    soma_pares += num1
+if num2 % 2 == 0:
+    soma_pares += num2
+if num3 % 2 == 0:
+    soma_pares += num3
+if num4 % 2 == 0:
+    soma_pares += num4
+if num5 % 2 == 0:
+    soma_pares += num5
+if num6 % 2 == 0:
+    soma_pares += num6
+print(f'A soma dos números pares é: {soma_pares}')
