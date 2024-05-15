eeb8d8cf19140f661c0b0bb8f1ade68a82134ada
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-14 21:14:00 -0300

    Update ex053.py

--- ex053.py
+++ ex053.py
@@ -2,3 +2,16 @@
 # e diga se ela é um palíndromeo, desconsiderando os espaços.
 # EX:APOS A SOPA / A SACADA DA CASA /A TORRE DA DERROTA /
 # O LOBO AMA BOLO / ANOTARAM A DATA DA MATATONA
+frase =  str(input('Digite uma frase: ')).strip().upper()
+#.strip() ->tira o espaço entes e depois /.upper() -> deixa tudo maiúculo
+palavras = frase.split() #separa nao vetor/lista
+junto = ''.join(palavras)# deixa tudo em uma string
+'''inverso = '''''
+inverso = junto[::-1]
+'''for letra in range(len(junto) - 1, -1, -1):#inverter a palavra/frase
+    inverso += junto[letra]'''
+print(f'O inverso de {junto} é {inverso}.')
+if inverso == junto:# testar ve se inverso é igual a junto
+    print('Temos um palíndromo!')#imprime tela se frase foi igual invertida
+else:
+    print('A frase digitada não é um palíndromo!')
