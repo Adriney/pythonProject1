9939f9cae1f052af8ef6311a48e4c2563a7a45cd
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-14 21:13:20 -0300

    Update ex051.py

--- ex051.py
+++ ex051.py
@@ -1,3 +1,12 @@
 #Desenvolva um programa que leia o primeiro termo e a razão de uma
 # PA(progressão aritmética). No final, mostre os 10 primeiros
 # termos dessa progressão.
+print('=='*12)
+print('  10 TERMOS DE UMA PA  ')
+print('=='*12)
+primeiro = int(input('Primeiro termo:'))
+razão = int(input('Razão:'))
+décimo = primeiro + (10 - 1) * razão
+for c in  range(primeiro, décimo + razão, razão):
+    print(f'{c}', end='-> ')
+print('ACABOU')
