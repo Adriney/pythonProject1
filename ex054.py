37a483814fbeadbf1cfa9c1b8c8278034d16043c
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-15 11:17:47 -0300

    Update ex054.py

--- ex054.py
+++ ex054.py
@@ -1,3 +1,14 @@
 #Crie um programa que leia o ano de nascimento de sete pessoas.
 # No final, mostre quantas pessoas ainda não atingiram a
-# maioridade e quantas jã são maiores.
+# maioridade e quantas já são maiores.
+menor = 0
+maior = 0
+for c in range(1, 8):
+    ano_nasc = int(input(f'Em que ano a {c}ª a pessoa nasceu? '))
+    idade = 2024 - ano_nasc
+    if idade < 18 :
+        menor += 1
+    else:
+        maior +=1
+print(f'{menor} Pessoas ainda não atingiram a maioridade!')
+print(f'{maior} Pessoas já maiores de idade!')
