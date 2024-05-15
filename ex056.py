ca7d8f6841528eb7930de92c25222563ad698fbf
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-15 11:18:15 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -3,3 +3,27 @@
 # A média de idade do grupo.
 # Qual é o nome do homem mais velho.
 # Quantas mulheres têm menos de 20 anos.
+
+somaidade = 0
+médiaidade = 0
+mairidadehomen = 0
+nomevelho = ''
+totmulher20 = 0
+for p in range (1, 5):
+    print(f'----- {p}ª PESSOA -----')
+    nome = str(input('Nome: ')).strip().upper()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip().upper()
+    somaidade += idade
+    if p == 1 and sexo == 'M':
+        mairidadehomen = idade
+        nomevelho = nome
+    if sexo in 'M' and idade > mairidadehomen:
+        mairidadehomen = idade
+        nomevelho = nome
+    if sexo in 'F' and idade < 20:
+        totmulher20 += 1
+médiaidade = somaidade / 4
+print(f'A média de idade do grupo é de {médiaidade} anos')
+print(f'O homen mais velho tem {mairidadehomen} anos, e se chama {nomevelho}.')
+print(f'Ao todo são {totmulher20} mulhere(s) com menos de 20 anos.')
