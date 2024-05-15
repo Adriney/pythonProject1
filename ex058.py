d3916f116dfa4961cf95fd05f0bb1680cf0ead38
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-15 11:18:45 -0300

    Create ex058.py

---
+++ ex058.py
@@ -0,0 +1,4 @@
+#Melhore o jogo do DESAFIO 028onde o computador vai "pensar"
+# em um número entre 0 a 10. Só que agora o jogador vai tentar
+# advinhar até acertar, mostrando no final quantos palpites
+# foram necessários para vencer.
