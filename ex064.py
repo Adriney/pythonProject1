f1215a6716364823eec3b97f488f8f8e86181fea
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-19 22:05:29 -0300

    Update ex064.py

--- ex064.py
+++ ex064.py
@@ -2,3 +2,11 @@
 # O programa só vai parar quando o usuário digitar o valor 999,
 # que é a condição de parada. No final, mostre quantos números
 # foram digitados e qual foi a soma entre eles (desconsiderando o flog).
+soma = cont = 0
+while True:
+    num = int(input('Digite um número \n(ou 999 para parar):\n'))
+    if num == 999:
+        break
+    soma += num
+    cont += 1
+print(f'Foram digitados {cont} números e a soma deles é {soma}.')
