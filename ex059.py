c7d68d66f78a8208e80badf2c6fc94475a15e800
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-19 21:19:42 -0300

    Update ex059.py

--- ex059.py
+++ ex059.py
@@ -5,3 +5,34 @@
 # [4] novos números
 # [5] sair do programa
 # Seu programa deverá realizar a operação solicitada em cada caso.
+while True:
+    op = int(input('Qual o tipo de operação deseja fazer:'
+                     '\n[1] somar '
+                     '\n[2] multiplicar '
+                     '\n[3] maior '
+                     '\n[4] novos números '
+                     '\n[5] sair do programa \n' ))
+
+
+    if op == 1:
+        num1 = int(input('Digite 1° número:'))
+        num2 = int(input('Digite 2° número:'))
+        soma = num1 + num2
+        print(f'A soma dos dois números é {soma}')
+    elif op == 2:
+        num1 = int(input('Digite 1° número:'))
+        num2 = int(input('Digite 2° número:'))
+        multiplicar = num1 * num2
+        print(f'A multipicação dos dois números é {multiplicar}')
+    elif op == 3:
+        num1 = int(input('Digite 1° número:'))
+        num2 = int(input('Digite 2° número:'))
+        maior = max(num1,num2)
+        print(f'O número maior entre os dos dois números é {maior}')
+    elif op == 4:
+        continue
+    elif op == 5:
+        print('Saindo do programa...')
+        break
+    else:
+        print('Opção inválida. Por favor, escolha uma opção válida.')
